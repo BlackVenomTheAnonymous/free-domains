192ff478cca35980b70150ac4e32ff043d532cd7
BlackVenomTheAnonymous <likhons304@gmail.com>
2023-05-17 09:25:24 +0600

    Create xerrox.is-an.app.js

---
+++ domains/xerrox.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: '⏤͟͞𝐗𝐄𝐑𝐑𝐎𝐗 🇧🇩 黑|•',
+  domain: 'is-an.app',
+  subdomain: 'xerrox',
+  owner: {
+    repo: 'https://github.com/yopremium21/abhirockz',
+    email: 'likhonexe@gmail.com',
+  },
+  record: {
+    CNAME: 'ogv84wpu.up.railway.app',
+  },
+  proxy: true,
+})
